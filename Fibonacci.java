89de4ecd1918628be89e71b6a699ea32e71320c6
Dasilvaj <dasilvaj1122@gmail.com>
2019-11-12 14:36:54 -0800

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,9 +1,9 @@
 public class Fibonacci { // Recursive Method to generate Fibonacci numbers
-	public int fib (int N) {
-		if (N == 0){
+	public int fib(int N) {
+		if(N == 0){
 			return 0;
 	    	}
-	    	else if (N == 1){
+	    	else if(N == 1){
 	    		return 1;
 	    	}
 		int val = fib(N-1) + fib(N - 2);
